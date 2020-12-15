ad422dd90619505a9251f9009ea30cef8d3ea6c7
songponnnnn <76036942+songponnnnn@users.noreply.github.com>
2020-12-15 16:11:56 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -11,6 +11,12 @@
 <p>ชื่อหลักสูตรภาษาไทย: หลักสูตรวิทยาศาตร์บัณฑิต สาขาวิทายาการข้อมูล</br>ภาษาอังกฤษ: Bachelor of Science Program in Data Scince</p>
 
 <a href="https://www.w3schools.com">Visit W3Schools</a>
+<h2>The src Attribute</h2>
+
+<p>HTML images are defined with the img tag, and the filename of the image source is specified in the src attribute:</p>
+
+<img src="https://www.thairath.co.th/media/dFQROr7oWzulq5FZUEj8fCLuVEy5tU5kY9rXPFgX58txVsuvCAPHgpUwRttzB4tZyU4.jpg" width="700" height="400">
+
 
 </body>
 </html>
